73a49fb75723285950ed9231e94b4862d9770dd0
Edmund Dea <deae@oregonstate.edu>
2019-02-10 19:38:44 -0800

    1) Update project specs 2) Update header in definition SQL file. 3) Rename definition/manipulation file.
    Signed-off-by: Edmund Dea <deae@oregonstate.edu>

--- db/fp_db_dump.sh
+++
@@ -1,1 +0,0 @@
-mysqldump -h classmysql.engr.oregonstate.edu -u cs340_deae -p cs340_deae fp_sector fp_order fp_order_type fp_portfolio fp_price fp_stock fp_user fp_user_stock > fp_db.sql
---
+++ db/fp_definition_dump.sh
@@ -0,0 +1,1 @@
+mysqldump -h classmysql.engr.oregonstate.edu -u cs340_deae -p cs340_deae fp_sector fp_order fp_order_type fp_portfolio fp_price fp_stock fp_user fp_user_stock > fp_definition_queries.sql
